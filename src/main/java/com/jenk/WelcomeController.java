066f3d57fd35675dd2b7ffe9a76c3b7c6a86af23
Pratham30jn <prathamjain7789@gmail.com>
2024-04-11 09:00:45 +0530

    commit

--- src/main/java/com/jenk/WelcomeController.java
+++ src/main/java/com/jenk/WelcomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	@GetMapping
+	@GetMapping("/get")
 	public String get() {
-		return "Hello World";
+		return "Hello Pratham";
 	}
 
 }
